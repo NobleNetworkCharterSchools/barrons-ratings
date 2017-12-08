523f481f9052ff644c1f18dee0703420c4bfcd9b
edwin-vallecillo <edwin.vallecillo@speeracademy.org>
2017-12-08 14:06:39 -0600

    Print names of colleges

--- pull_barrons_info.py
+++ pull_barrons_info.py
@@ -1,14 +1,28 @@
-pull_barrons_infoFile = open("/home/pi/projects/barrons/data/downs/searchpage-profile.cfm_001.html")
+
+import os, bs4, csv 
+directory = os.listdir("/home/pi/projects/barrons/data/downs")
+for file in directory:
+    if file.endswith(".html"):
+        pull_barrons_infoFile = open(file)
+        pull_barrons_infoContent = pull_barrons_infoFile.read()
+        pull_barrons_infoSoup = bs4.BeautifulSoup(pull_barrons_infoContent, "html.parser")
+        th = pull_barrons_infoSoup.select('html #search-profile #page-wrapper div#content-wrapper div#searchleftcol div.searchcontent table.tbl-profile tbody.basic-info tr th')
+#---------------------------------------------------------------------------------------------------------------------------------------------------------
+    # Put the names into a spreadsheet.
+#Take the names from the output, separate them by commas(somehow?) plave them in a spreadsheet. 
+        #td = pull_barrons_infoSoup.select('html #search-profile #page-wrapper #content-wrapper #searchleftcol div table tbody.basic-info tr td')
+        #print(td)
+        print(th)
 #print(dir(pull_barrons_infoFile))
-pull_barrons_infoContent = pull_barrons_infoFile.read()
+##pull_barrons_infoContent = pull_barrons_infoFile.read()
 #print(pull_barrons_infoContent) #Prints Everything 
-import bs4
-pull_barrons_infoSoup = bs4.BeautifulSoup(pull_barrons_infoFile.read(), "html.parser")
-print(type(pull_barrons_infoSoup))
-print(pull_barrons_infoSoup.prettify())
-#th = pull_barrons_infoSoup.select('html body#search-profile.searchpage div#page-wrapper div#content-wrapper div#searchleftcol div.searchcontent table.tbl-profile tbody.basic-info tr')
-#td = pull_barrons_infoSoup.select('html body#search-profile.searchpage div#page-wrapper dic#content-wrapper div#searchleftcol div.searchcontent table.tbl-profile tbody.basic-info tr td')
-td = pull_barrons_infoSoup.select('html')# body div div div div table tbody tr td')
-print(td)
+#import bs4
+#pull_barrons_infoSoup = bs4.BeautifulSoup(pull_barrons_infoContent, "html.parser")
+#print(type(pull_barrons_infoSoup))
+        #print(file)
+#th = pull_barrons_infoSoup.select('html #search-profile #page-wrapper div#content-wrapper div#searchleftcol div.searchcontent table.tbl-profile tbody.basic-info tr th')
+    #td = pull_barrons_infoSoup.select('html #search-profile #page-wrapper #content-wrapper #searchleftcol div table tbody.basic-info tr td')
+#td = pull_barrons_infoSoup.select('html')#body div div div div table tbody tr td')
+#print(td)
 #print(th)
       
