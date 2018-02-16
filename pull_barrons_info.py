0b1dad890f50c7854dc0de78c9a92469c95f0161
edwin-vallecillo <edwin.vallecillo@speeracademy.org>
2018-02-16 13:29:41 -0600

    Files that only had an ACT score were reworked to state that no competitivenes was available.

--- pull_barrons_info.py
+++ pull_barrons_info.py
@@ -5,7 +5,7 @@ DATADIR = "/home/pi/projects/barrons/data/downs_full"
 
 input_files_names = os.listdir(DATADIR)
 csvFile = open('college.csv', 'w', newline='')
-csvWriter = csv.writer(csvFile)#, delimiter='\t', lineterminator='\n\n')
+csvWriter = csv.writer(csvFile)
 csvWriter.writerow(['College Name', 'Selectivity', 'file_name'])
 print("starting")
 
@@ -20,14 +20,14 @@ for file_name in input_files_names:
         try:
             school_name = th_elements[-1].text
             #For a handfull of files, a semicolon is not present in the html file, but gets inserted. 
-            school_name = school_name.replace(";", "") 
+            school_name = school_name.replace(";", "")
         except IndexError:
             continue
             print(file_name)
             school_name = None
 #---------------------------------------------------------------------------------------------------------------------------------------------------------
     # Put the names into a spreadsheet.
-#Take the names from the output, separate them by commas(somehow?) plave them in a spreadsheet. 
+#Take the names from the output, separate them by commas(somehow?) place them in a spreadsheet. 
         td_elements = pull_barrons_infoSoup.select('html #search-profile #page-wrapper #content-wrapper #searchleftcol div table tbody.basic-info tr td')
         reader = csv.reader(file)
 #takes the data in td and forms a list with it 
@@ -39,8 +39,10 @@ for file_name in input_files_names:
         except IndexError:
 #if there is no values for td in the file then the code prints "unknown value"
             print("Not Available")
-            competitivenes = None 
-        
+            competitivenes = None
+        #Some files only had an ACT as final list object. No other competitivesnes rating available.
+        if competitivenes.startswith("ACT:"):
+            competitivenes = "Not Available"
         csvWriter.writerow([school_name, competitivenes, file_name])
 print("Finished")
 csvFile.close()
